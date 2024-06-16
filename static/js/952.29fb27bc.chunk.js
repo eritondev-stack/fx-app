8c65bdceacc90e2270535afa7ecb610013756c16
Eriton Gomes De Souza <eriton.gomes.souza@gmail.com>
2024-06-16 01:10:56 -0300

    forex

---
+++ static/js/952.29fb27bc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfx_app=self.webpackChunkfx_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.29fb27bc.chunk.js.map
